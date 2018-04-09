e8d9026bdd7e5fafd2641f24d3c0ec5c1a953a55
Kyle Moffett <kyle.moffett@hotmail.com>
2018-04-09 14:50:13 -0700

    Added exception for player and match not found

--- commands/pubg.py
+++ commands/pubg.py
@@ -15,21 +15,24 @@ class Battlegrounds():
         self.api = PUBG(self.api_key, Shard.PC_NA)
         self.bot = bot
 
-
     @commands.command(pass_context=True)
-    async def test(self, ctx):
-        shroud = self.api.players().filter(player_names=['shroud'])[0]
-        shrouds_last = self.api.matches().get(shroud.matches[0].id)
-        print (shrouds_last) 
-
-    @commands.command(pass_context=True)
-    async def last(self, ctx): 
+    async def last(self, ctx, supplied_name=None): 
         """Retrieves the stats of the last game played"""
         if getGameName(ctx.message.author.name)[0] == 0:
             pubg_name = getGameName(ctx.message.author.name)[1]
+            if supplied_name:
+                pubg_name = supplied_name
             print ("Searching for {}'s last game".format(pubg_name))
-            player = self.api.players().filter(player_names=[pubg_name])[0]
-            last_match = self.api.matches().get(player.matches[0].id)
+            player = None
+            try:
+                player = self.api.players().filter(player_names=[pubg_name])[0]
+            except Exception:
+                await self.bot.say("{} not found".format(pubg_name))
+                return
+            try:
+                last_match = self.api.matches().get(player.matches[0].id)
+            except Exception:
+                await self.bot.say("No recent matchs for {}".format(pubg_name))
             player_found = False
             for roster in last_match.rosters:
                 for participant in roster.participants:
@@ -38,7 +41,7 @@ class Battlegrounds():
                         print (participant.name + "Game Found")
                         em = discord.Embed(colour = discord.Colour.orange())
                         em.title = "Stat's for {}'s last game".format(pubg_name)
-                        em.add_field(name='Match Time', value=last_match.created_at, inline=True)
+                        em.add_field(name='Match Type', value=last_match.game_mode, inline=True)
                         em.add_field(name='Match Duration', value=last_match.duration, inline=True)
                         em.add_field(name='Finishing Place', value=participant.win_place, inline=True)
                         em.add_field(name='Kills', value=participant.kills, inline=True)
