8b91c21ede36b45d186064e6efb553c8bccd1a51
Kyle Moffett <kyle.moffett@hotmail.com>
2018-04-11 15:13:10 -0700

    added embedStats method

--- commands/pubg.py
+++ commands/pubg.py
@@ -2,8 +2,6 @@ from misc.helper import getGameName
 from discord.ext import commands
 from pubg_python import PUBG, Shard
 import discord
-import re
-import requests
 import configparser
 
 class Battlegrounds():
@@ -14,6 +12,21 @@ class Battlegrounds():
         self.api_key = defaultConfig['api_key']
         self.api = PUBG(self.api_key, Shard.PC_NA)
         self.bot = bot
+
+    def embedStats(self, match, participant):
+        """Take in player and match objects to be embedded for message"""
+        em = discord.Embed(colour = discord.Colour.orange())
+        em.title = "Stat's for {}'s last game".format(participant.name)
+        em.add_field(name='Match Type', value=match.game_mode, inline=True)
+        em.add_field(name='Match Duration', value=match.duration, inline=True)
+        em.add_field(name='Finishing Place', value=participant.win_place, inline=True)
+        em.add_field(name='Kills', value=participant.kills, inline=True)
+        em.add_field(name='Assists', value=participant.assists, inline=True)
+        em.add_field(name='Headshot Kills', value=participant.headshot_kills, inline=True)
+        em.add_field(name='Walk Distance', value=str(participant.walk_distance) + "m", inline=True)
+        em.add_field(name='Ride Distance', value=str(participant.ride_distance) + "m", inline=True)
+        em.add_field(name='Team Kills', value=participant.team_kills, inline=True)
+        return em    
 
     @commands.command(pass_context=True)
     async def last(self, ctx, supplied_name=None): 
@@ -46,17 +59,7 @@ class Battlegrounds():
                 if participant.name == pubg_name:
                     player_found = True
                     print (participant.name + "Game Found")
-                    em = discord.Embed(colour = discord.Colour.orange())
-                    em.title = "Stat's for {}'s last game".format(pubg_name)
-                    em.add_field(name='Match Type', value=last_match.game_mode, inline=True)
-                    em.add_field(name='Match Duration', value=last_match.duration, inline=True)
-                    em.add_field(name='Finishing Place', value=participant.win_place, inline=True)
-                    em.add_field(name='Kills', value=participant.kills, inline=True)
-                    em.add_field(name='Assists', value=participant.assists, inline=True)
-                    em.add_field(name='Headshot Kills', value=participant.headshot_kills, inline=True)
-                    em.add_field(name='Walk Distance', value=str(participant.walk_distance) + "m", inline=True)
-                    em.add_field(name='Ride Distance', value=str(participant.ride_distance) + "m", inline=True)
-                    em.add_field(name='Team Kills', value=participant.team_kills, inline=True)
+                    em = self.embedStats(last_match, participant)
                     await self.bot.send_message(ctx.message.channel, embed=em)
                     break
         if player_found == False:
